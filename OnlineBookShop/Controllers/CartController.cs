dc4c2cc1c094fde1cb79ad5d326a1747838c48f2
Luong Quoc Khang <khang.luong@kyanon.digital>
2018-10-11 19:40:56 +0700

    - ajax ( increase , decrease cartitem quantity , delete cartitem )

--- OnlineBookShop/Controllers/CartController.cs
+++ OnlineBookShop/Controllers/CartController.cs
@@ -20,12 +20,13 @@ namespace OnlineBookShop.Controllers
             using (var db = new DBContext())
             {
                 var user = (UserLogin)Session[Constants.USER_SESSION];
-                if ( user != null )
+                if (user != null)
                 {
                     Cart cart = db.Cart.FirstOrDefault(x => x.UserName == user.UserName);
                     int cartid = cart.CartId;
                     var cartdetail = db.CartDetail.Where(x => x.CartId == cartid).ToList();
                     ViewBag.CartDetails = cartdetail;
+
 
                     List<Book> listbook = new List<Book>();
                     foreach (var item in cartdetail)
@@ -41,6 +42,44 @@ namespace OnlineBookShop.Controllers
                     return new RedirectToRouteResult(new RouteValueDictionary { { "Controller", "User" }, { "Action", "Login" } });
                 }
             }
+        }
+        public ActionResult ChangeQuantity()
+        {
+            string bookid = "", action = "";
+            try { bookid = Request.Form["bookid"]; } catch (Exception e) { }
+            try { action = Request.Form["action"]; } catch (Exception e) { }
+
+            using (var db = new DBContext())
+            {
+                var User = (UserLogin)Session[Constants.USER_SESSION];
+                int cartid = db.Cart.FirstOrDefault(x => x.UserName == User.UserName).CartId;
+                CartDetails book = db.CartDetail.FirstOrDefault(x => x.BookId == bookid && x.CartId == cartid);
+                if (action == "Increase")
+                {
+                    book.Quantity += 1;
+                }
+                else if (action == "Decrease")
+                {
+                    book.Quantity -= 1;
+                }
+                db.SaveChanges();
+                return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult DeleteCart(string bookid)
+        {
+            using (var db = new DBContext())
+            {
+                var User = (UserLogin)Session[Constants.USER_SESSION];
+                int cartid = db.Cart.FirstOrDefault(x => x.UserName == User.UserName).CartId;
+                CartDetails book = db.CartDetail.FirstOrDefault(x => x.BookId == bookid && x.CartId == cartid);
+                if (book != null)
+                {
+                    db.CartDetail.Remove(book);
+                    db.SaveChanges();
+                }
+            }
+            return Json(new { result = true }, JsonRequestBehavior.AllowGet);
         }
     }
 }
